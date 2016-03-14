27703d42fa2406f9bd9fad9743f0e16201dbdba5
campari <hrubyjos@gmail.com>
2016-03-14 17:13:49 +0100

    fixes

--- www/js/main.js
+++ www/js/main.js
@@ -115,10 +115,19 @@ function demoBasic(){
                     days = Math.floor(t / 86400);
                     t -= days * 86400;
                     hours = Math.floor(t / 3600) % 24;
+                    if(hours < 10){
+                        hours = "0" + hours;
+                    }
                     t -= hours * 3600;
                     minutes = Math.floor(t / 60) % 60;
+                     if(minutes < 10){
+                        minutes = "0" + minutes;
+                    }
                     t -= minutes * 60;
                     seconds = t % 60;
+                    if(seconds < 10){
+                        seconds = "0" + seconds;
+                    }
                     return [
                         days + 'd',
                         hours + 'h',
